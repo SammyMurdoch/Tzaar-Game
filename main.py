4c6dd33245107f01113b7eed62480cec5697103e
SammyMurdoch <sammy.murdoch.0@gmail.com>
2022-11-14 15:29:18 +0000

    Can now move pieces

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import random
 from numpy import *
 
 
-def generate_connections(node, nodes):
-    directions = [(-1, -1), (-1, 0), (0, -1), (0, 1), (1, 0), (1, 1)]
-
-    connections = [(node[0] + dir[0], node[1] + dir[1]) for dir in directions if (node[0] + dir[0], node[1] + dir[1]) in nodes]
+def generate_connections(node, nodes, directions):
+    connections = [(node[0] + dir[0], node[1] + dir[1]) if (node[0] + dir[0], node[1] + dir[1]) in nodes else None for dir in directions]
 
     return connections
 
 
-def generate_board_dict(piece_data, side_length=5):
+def generate_board_dict(piece_data, directions, side_length=5):
     max_row_length = 2 * side_length - 1
 
     board_keys = [(x, y) for x in range(max_row_length) for y in range(max_row_length - abs(x-side_length+1))]
@@ -19,40 +17,45 @@ def generate_board_dict(piece_data, side_length=5):
     piece_order = [key for key, value in piece_data.items() for i in range(value)]
     random.shuffle(piece_order)
 
-    connection_data = [generate_connections(node, board_keys) for node in board_keys]
+    node_data = [[piece, 1] for piece in piece_order]
+    node_dict = dict(zip(board_keys, node_data))
+    
+    neighbour_data = [generate_connections(node, board_keys, directions) for node in board_keys]
+    neighbour_dict = dict(zip(board_keys, neighbour_data))
 
-    node_data = [[piece, 1, connection_data[i]] for i, piece in enumerate(piece_order)]
-
-    board = dict(zip(board_keys, node_data))
-
-    return board
-
-
-def move_piece(start, end, board):
-    board[end] = board[start]
-    board[start] = [None, 0]
-
-    return board
+    return node_dict, neighbour_dict
 
 
-def stack_piece(start, end, board):
-    stack_height = board[start][1]
+def move_piece(start, end, nodes, neighbours):
+    nodes[end] = nodes[start]
+    nodes[start] = [None, 0]
 
-    board = move_piece(start, end, board)
-    board[end][1] += stack_height
+    for d, neighbour in enumerate(neighbours[start]):
+        if neighbour is not None:
+            neighbours[neighbour][(d+3) % 6] = neighbours[start][(d+3) % 6]
 
-    return board
+    return nodes, neighbours
 
 
+def stack_piece(start, end, nodes, neighbours):
+    stack_height = nodes[start][1]
+
+    nodes, neighbours = move_piece(start, end, nodes, neighbours)
+    nodes[end][1] += stack_height
+
+    return nodes, neighbours
 
 
 piece_data = {"W-Tzaar": 6, "W-Tzaara": 9, "W-Tott": 15, "B-Tzaar": 6, "B-Tzaara": 9, "B-Tott": 15}
+directions = [(-1, -1), (-1, 0), (0, 1), (1, 1), (1, 0), (0, -1)]
 
-board = generate_board_dict(piece_data)
+board, neighbours = generate_board_dict(piece_data, directions)
 
-print(board)
+print(neighbours)
 
-move_piece((0, 0), (0, 1), board)
-stack_piece((0, 2), (0, 3), board)
+board, neighbours = move_piece((0, 1), (0, 0), board, neighbours)
+board, neighbours = stack_piece((0, 2), (0, 3), board, neighbours)
 
-print(board)
+print(neighbours)
+
+print(neighbours[(0, 2)])
